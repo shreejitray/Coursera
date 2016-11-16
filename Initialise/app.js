86afe8d495e04c1bad034ae06eb6d7db46c5fb5c
Shreejit Chaudhury <SChaudhury@walmartlabs.com>
2016-11-16 10:50:56 +0530

    first commit

---
+++ Initialise/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by schaud3 on 11/15/16.
+ */
+(function(){
+    'use strict';
+  angular.module('myFirstApp', [])
+      .controller('MyFirstController', function(){
+
+      });
+})();
